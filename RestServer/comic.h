0a7ff25b87316b2d04e0fb7cf20f7ca434c41be0
prajwaldas95 <prajwaldas95@gmail.com>
2022-07-21 13:57:30 -0500

    first commit

---
+++ RestServer/comic.h
@@ -0,0 +1,24 @@
+#pragma once
+#pragma warning( disable : 4996)
+#include <string>
+namespace comicsdb
+{
+	struct Comic
+	{
+		enum
+		{
+			DELETED_ISSUE = -1
+		};
+		std::string title;
+		int issue{ DELETED_ISSUE };
+		std::string writer;
+		std::string penciler;
+		std::string inker;
+		std::string letterer;
+		std::string colorist;
+
+	};
+	std::string toJson(const Comic& comic);
+	Comic fromJson(const std::string& json);
+
+}// namespace comicsdb
